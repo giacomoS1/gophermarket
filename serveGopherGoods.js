e92c9f058fa36f4323433cae7ad09966d3cf5d31
ChickenWingnut <chungN24799@gmail.com>
2024-09-24 11:00:09 -0500

    Added Comments/Info
    Created some basic info on the page for people to understand the boilerplate for our server file.

--- serveGopherGoods.js
+++ serveGopherGoods.js
@@ -1,14 +1,24 @@
+// Imports are packages that are used in a js file. We will be using something called ES6, which imports node modules a bit differently compared to old methods.
+/* Example of old method: 
+var express = require("express")
+-- Please make sure that when you are learning node, that you use import, as using require will not work with our server.
+*/
 import express from "express";
 
+// This creates a variable called app, which will be used to call to our server. Use it to establish http requests
 const app = express();
 // Port set here
 const PORT = 3000;
-// Sets destination for all ejs files
+// Sets destination for all ejs files to the directory app/views.
+// Ejs applications use something called a views folder to hold all the main html pages we plan to use throughout the website. They will be formatted nearly exactly like the basic html page.
 app.set('views', './app/views');
 
-
+/*This is the first, basic http request. We will be using the main http requests to make a CRUD app.
+*/
 app.get("/", (req, res) => {
+// req stands for request, res stands for response. Requests are used to ask for information from a user/create an input while responses are going to be used to send data to the user. This can range from certain lists of objects, to pull pages like the one below
   res.render("index.ejs");
+
 });
 
 app.listen(PORT, () => {
