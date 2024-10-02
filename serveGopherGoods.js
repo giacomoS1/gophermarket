eb39cef692c8a6e92445ad5eb8150d8679ab51a3
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-10-02 02:15:54 -0500

    Added google OAuth functionality, which only works with @umn.edu emails. Make sure to add the client secret shared in the discord before attempting to use

--- serveGopherGoods.js
+++ serveGopherGoods.js
@@ -4,7 +4,9 @@ var express = require("express")
 -- Please make sure that when you are learning node, that you use import, as using require will not work with our server.
 */
 import express from "express";
-
+import passport from "passport"; // for google oauth
+import {Strategy as GoogleStrategy} from "passport-google-oauth20"; // for google oauth
+import session from "express-session"; // for google oauth
 
 // Imports below will be used for Routing pages
 import browseRoute from "./app/routes/browse.js";
@@ -14,6 +16,37 @@ import browseRoute from "./app/routes/browse.js";
 const app = express();
 // Port set here
 const PORT = 3000;
+
+// configures express-session
+app.use(session({
+  secret: 'asdgasdgfsadgfdsgsdfdsgfagfdsgdsfgfdsgfsdsdf', // placeholder - not secure!
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // set to true once using https
+}));
+
+// initializes passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+// google oauth setup
+passport.use(new GoogleStrategy({
+  clientID: '994706186413-h03c8po5lklpnsp7dpmgl47ougg29rm4.apps.googleusercontent.com',
+  clientSecret: '', // insert client secret here from discord before trying
+  callbackURL: '/auth/google/callback',
+  scope: ['profile', 'email']
+}, (accessToken, refreshToken, profile, done) => {
+  // saving user info to dababase can be done here
+  done(null, profile) // passes use profile for now
+}));
 
 /* Static files are the local files that are going to be referenced ex:Images */
 app.use(express.static('./css'))
@@ -31,9 +64,42 @@ app.get("/", (req, res) => {
 
 });
 
-app.get("/account", (req,res) => {
+// google oauth login route
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 
-})
+// google oauth callback route
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/'}),
+  (req, res) => {
+    // successful authentication
+    res.redirect('/account');
+  }
+);
+
+// account route - protected page
+app.get('/account', isLoggedIn, (req, res) => {
+  res.send(`<h1>Account<h1><p>Hello, ${req.user.displayName}</p><a href="/logout">Logout</a>`);
+});
+
+// logout route
+app.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+// checks if user is authenticated
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
 
 app.listen(PORT, () => {
   console.log(`Gopher is running at http://localhost:${PORT}/`);
