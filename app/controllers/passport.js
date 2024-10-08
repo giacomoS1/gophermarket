49304f112a53ca424986f82963a7b8204c5cd93e
Caden Jurkovski <jurko036@umn.edu>
2024-10-08 16:36:21 -0500

    Added function for adding new user info to database upon authentication

--- app/controllers/passport.js
+++ app/controllers/passport.js
@@ -1,5 +1,6 @@
 import passport from "passport";
-import dotenv from "dotenv/config";
+import "dotenv/config";
+import pool from "../database/config.js";
 
 import {Strategy as GoogleStrategy} from "passport-google-oauth20"; // for google oauth
 
@@ -10,9 +11,29 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
     callbackURL: '/auth/google/callback',
     scope: ['profile', 'email']
-  }, (accessToken, refreshToken, profile, done) => {
-    // saving user info to dababase can be done here
-    done(null, profile) // passes use profile for now
+  }, async (accessToken, refreshToken, profile, done) => { // waits for information to be fetched
+    try {
+      const email = profile.emails[0].value;
+      const firstName = profile.name.givenName;
+      const lastName = profile.name.familyName; // retrieves email, first name, and last name from google auth
+
+      const userField = await pool.query(
+        'SELECT * FROM users WHERE email = $1', [email] // grabs row where the users email exists (if it does at all)
+      );
+      if (userField.rows.length === 0) {  // checks to see if user actually existed or not
+        const newUser = await pool.query(
+          'INSERT INTO users (email, first_name, last_name) VALUES ($1, $2, $3) RETURNING *', [email, firstName, lastName] // inserts new user info
+        );
+        console.log("New user created:", newUser.rows[0]); 
+        done(null, newUser.rows[0]); // returns user, null signifies that there wasn't an error here
+      } else {
+        console.log("User already exists:", userField.rows[0]);
+        done(null, userField.rows[0]);
+      }
+    } catch (err) {
+      console.error("Error saving user to the database", err);
+      done(err, null);
+    }
   }));
 
   passport.serializeUser((user, done) => {
--- app/database/config.js
+++ app/database/config.js
@@ -1,11 +1,17 @@
-import express from express
-import dotenv from "dotenv/config";
+import "dotenv/config";
+import pkg from "pg";
 
-var config =
-    {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-    }
+const { Pool } = pkg; // this seems a little redundant, but it needs to be done because of how pg works as a commonjs module with exports
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    max: 10, // the maximum number of users in pool at one time
+    idleTimoutMillis: 3000, // closes idle pool connections after 30 seconds
+    connectionTimeoutMillis: 2000 // waits 2 seconds for connection before throwing an error
+});
+
+export default pool;
