c2d3f2c54a24fd0838780aae4926d69a54baa3ea
Caden Jurkovski <jurko036@umn.edu>
2024-10-28 19:19:38 -0500

    Added simple form in account page

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { isLoggedIn } from "../controllers/authentication.js";
+import { addProduct } from "../database/productService.js";
+import bodyParser from 'body-parser';
 
 const app = express.Router();
 
+// parses url bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // account route - protected page
 app.get('/', isLoggedIn, (req, res) => {
-    console.log(req);
-    res.render("account.ejs", { name: req.user.displayName, logout: false});
+    res.render("account.ejs", { name: req.user.displayName, logout: false });
 });
-
 
 // logout route
 app.get('/logout', (req, res) => {
@@ -18,7 +21,19 @@ app.get('/logout', (req, res) => {
             return next(err);
         }
     });
-    res.render("account.ejs", {logout: true})
+    res.render("account.ejs", { logout: true });
+});
+
+// route to handle product listing form submission
+app.post('/list-product', isLoggedIn, async (req, res) => {
+    const { productName, price } = req.body;
+    try {
+        await addProduct(productName, price, req.user.id);
+        res.redirect('/account');
+    } catch (err) {
+        console.error("Error listing product", err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 export default app;
